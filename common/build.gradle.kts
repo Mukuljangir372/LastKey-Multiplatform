8e558cb202e1b1c504c4a8c5864b010137e6cfea
Mukul Jangir <mukul.jangir372@gmail.com>
2023-05-01 21:07:29 +0530

    ios

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,4 +1,4 @@
-import org.jetbrains.compose.compose
+@file:Suppress("DEPRECATION", "UnstableApiUsage")
 
 plugins {
     kotlin("multiplatform")
@@ -11,9 +11,21 @@ version = "1.0-SNAPSHOT"
 
 kotlin {
     android()
+
     jvm("desktop") {
         jvmToolchain(11)
     }
+
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach {
+        it.binaries.framework {
+            baseName = "common"
+        }
+    }
+
     sourceSets {
         val commonMain by getting {
             dependencies {
@@ -29,21 +41,34 @@ kotlin {
         }
         val androidMain by getting {
             dependencies {
-                api("androidx.appcompat:appcompat:1.5.1")
-                api("androidx.core:core-ktx:1.9.0")
+                api("androidx.appcompat:appcompat:1.6.1")
+                api("androidx.core:core-ktx:1.10.0")
             }
         }
-//        val androidTest by getting {
-//            dependencies {
-//                implementation("junit:junit:4.13.2")
-//            }
-//        }
         val desktopMain by getting {
             dependencies {
                 api(compose.preview)
             }
         }
         val desktopTest by getting
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+        val iosTest by creating {
+            dependsOn(commonTest)
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
+        }
     }
 }
 
