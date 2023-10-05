4d8b0ece6a5b6bce041a33ba87d82e0d8f387188
mukuljangir372 <mukul.jangir372@gmail.com>
2023-10-05 22:56:16 +0530

    setup ui

--- core-ui/build.gradle.kts
+++ core-ui/build.gradle.kts
@@ -1,14 +1,45 @@
+@file:Suppress("UnstableApiUsage")
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
     id("com.mu.lastkey.kmm")
+    alias(libs.plugins.compose)
 }
 kotlin {
     sourceSets {
+        val androidMain by getting {
+            dependencies {
+                api(compose.uiTooling)
+                api(compose.preview)
+                api(libs.androidx.window)
+            }
+        }
         val commonMain by getting {
             dependencies {
+                api(compose.runtime)
+                api(compose.foundation)
+                api(compose.material)
+                api(compose.material3)
+                api(libs.compose.material3.windowsize)
+                @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class)
+                api(compose.components.resources)
+                api(libs.bundles.voyager)
+                implementation(libs.kotlin.coroutines.core)
             }
+        }
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by getting {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
         }
     }
 }
 android {
     namespace = "com.mu.lastkey.core.ui"
+    buildFeatures {
+        compose = true
+    }
 }
