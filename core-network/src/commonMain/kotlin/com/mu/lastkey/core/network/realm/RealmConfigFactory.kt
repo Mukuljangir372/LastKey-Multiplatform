08d3550bacb323de5b2720d31711000487fdb08e
mukuljangir372 <mukul.jangir372@gmail.com>
2023-10-16 13:36:46 +0530

    minor changes

---
+++ core-network/src/commonMain/kotlin/com/mu/lastkey/core/network/realm/RealmConfigFactory.kt
@@ -0,0 +1,10 @@
+package com.mu.lastkey.core.network.realm
+
+class RealmConfigFactory {
+    fun create(): RealmConfig {
+        return RealmConfig(
+            appId = "primerapp-gkfdg",
+            apiKey = "v4oLzf0xzCUL9vvQZrFowXy9raFzpzO0eNFYmqmFkpdmMnk2ohi9aBXhgyUWCmgc"
+        )
+    }
+}
