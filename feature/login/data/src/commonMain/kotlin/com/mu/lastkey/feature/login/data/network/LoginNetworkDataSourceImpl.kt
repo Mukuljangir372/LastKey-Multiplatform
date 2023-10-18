fee3660d99dd1e0703c7b5226ef0ebe05dc3d1f2
mukuljangir372 <mukul.jangir372@gmail.com>
2023-10-18 15:16:40 +0530

    fix[Login]: timeout

--- feature/login/data/src/commonMain/kotlin/com/mu/lastkey/feature/login/data/network/LoginNetworkDataSourceImpl.kt
+++ feature/login/data/src/commonMain/kotlin/com/mu/lastkey/feature/login/data/network/LoginNetworkDataSourceImpl.kt
@@ -5,6 +5,7 @@ import com.mu.lastkey.feature.login.data.network.api.LoginApi
 import com.mu.lastkey.feature.login.data.network.api.UserApi
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.withTimeout
 
 class LoginNetworkDataSourceImpl(
     private val loginApi: LoginApi,
@@ -13,20 +14,28 @@ class LoginNetworkDataSourceImpl(
 ) : LoginNetworkDataSource {
     override suspend fun signIn(request: LoginApi.Request): LoginApi.Response {
         return withContext(coroutineDispatcher) {
-            loginApi.signIn(request)
+            withTimeout(TIMEOUT) {
+                loginApi.signIn(request)
+            }
         }
     }
 
     override suspend fun signUp(request: LoginApi.Request): LoginApi.Response {
         return withContext(coroutineDispatcher) {
-            loginApi.signUp(request)
-            val signInResponse = loginApi.signIn(request)
-            val user = UserRealmModel().apply {
-                _id = org.mongodb.kbson.ObjectId(signInResponse.loggedInUserId)
-                email = request.email
+            withTimeout(TIMEOUT) {
+                loginApi.signUp(request)
+                val signInResponse = loginApi.signIn(request)
+                val user = UserRealmModel().apply {
+                    _id = org.mongodb.kbson.ObjectId(signInResponse.loggedInUserId)
+                    email = request.email
+                }
+                userApi.createNewUser(user)
+                signInResponse
             }
-            userApi.createNewUser(user)
-            signInResponse
         }
+    }
+
+    companion object {
+        private const val TIMEOUT = 3000L
     }
 }
