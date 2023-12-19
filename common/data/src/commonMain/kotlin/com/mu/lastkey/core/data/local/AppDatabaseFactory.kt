bd75bb5ac119ef0ffff0fa8b13d65a51fb1640dd
mukuljangir372 <mukul.jangir372@gmail.com>
2023-12-19 17:55:28 +0530

    feat[Credential]: store

--- common/data/src/commonMain/kotlin/com/mu/lastkey/core/data/local/AppDatabaseFactory.kt
+++ common/data/src/commonMain/kotlin/com/mu/lastkey/core/data/local/AppDatabaseFactory.kt
@@ -2,12 +2,18 @@ package com.mu.lastkey.core.data.local
 
 import app.cash.sqldelight.db.SqlDriver
 import com.mu.lastkey.core.data.AppDatabase
+import com.mu.lastkey.core.database.getJsonColumnAdapter
 import com.mu.lastkey.core.database.getLocalDateTimeColumnAdapter
 import commulastkeycoredata.CredentialEntity
+import commulastkeycoredata.CredentialPagingEntity
 import commulastkeycoredata.CredentialSectionEntity
 import commulastkeycoredata.CredentialSectionFieldEntity
+import kotlinx.serialization.json.Json
 
-class AppDatabaseFactory(private val driver: SqlDriver) {
+class AppDatabaseFactory(
+    private val driver: SqlDriver,
+    private val json: Json
+) {
     fun create(): AppDatabase {
         return AppDatabase(
             driver = driver,
@@ -22,6 +28,9 @@ class AppDatabaseFactory(private val driver: SqlDriver) {
             credentialSectionFieldEntityAdapter = CredentialSectionFieldEntity.Adapter(
                 createdAtAdapter = getLocalDateTimeColumnAdapter(),
                 updatedAtAdapter = getLocalDateTimeColumnAdapter()
+            ),
+            credentialPagingEntityAdapter = CredentialPagingEntity.Adapter(
+                offsetIdsAdapter = getJsonColumnAdapter(json)
             )
         )
     }
